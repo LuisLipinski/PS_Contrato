0df72074892269b27280eef0ffb526fd2403a020
LuisLipinski <98597288+LuisLipinski@users.noreply.github.com>
2026-08-25 20:04:45 -0300

    feat: expose deployed commit version

---
+++ src/main/java/com/mypetadmin/ps_contrato/controller/VersionController.java
@@ -0,0 +1,13 @@
+package com.mypetadmin.ps_contrato.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+
+    @GetMapping("/version")
+    public String version() {
+        return System.getenv("RENDER_GIT_COMMIT");
+    }
+}
